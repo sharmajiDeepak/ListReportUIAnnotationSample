b504c6ecbef498c75cb8439a00dc67574b9d857b
Sharma Deepak <sharmaji.dipak@gmail.com>
2019-06-27 04:53:07 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.capgemini.deepak.listreportuiannotationsample.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.capgemini.deepak.listreportuiannotationsample.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
